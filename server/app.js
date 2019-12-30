5d4e24c9268b48d8e082873c2a2817f098a0b6b9
Keenan Turley <keenan.turley@gmail.com>
2019-12-29 23:19:36 -0500

    Implement example websocket

--- server/app.js
+++ server/app.js
@@ -1,15 +1,10 @@
-// Server example
-const http = require('http');
+const WebSocket = require('ws')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const wss = new WebSocket.Server({ port: 8080 })
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('ho!')
+})

025082aa36f57f62a46612cf2a615927c34a8b64
Keenan Turley <keenan.turley@gmail.com>
2019-12-30 05:17:39 -0500

    Add https and config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,23 @@
+const config = require("./config.json");
+const https = require('https');
+const fs = require('fs');
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+
+let wsOptions = { port: 8080 };
+
+if (config.useHttps) {
+    const options = {
+        key: fs.readFileSync(config.keyPath),
+        cert: fs.readFileSync(config.certPath)
+    };
+
+    wsOptions.server = https.createServer(options, function (req, res) {
+        res.writeHead(200);
+        res.end("hello world\n");
+    }).listen(8000);
+}
+
+const wss = new WebSocket.Server(wsOptions);
 
 wss.on('connection', ws => {
     ws.on('message', message => {

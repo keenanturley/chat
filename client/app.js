343e3ce1bb7ce4830d86a28e23ddfedc64001fe3
Keenan Turley <keenan.turley@gmail.com>
2019-12-29 23:22:46 -0500

    Add basic message handling

--- client/app.js
+++ client/app.js
@@ -1,1 +1,24 @@
-console.log("Hello, world!");
+const url = 'ws://localhost:8080/'
+const connection = new WebSocket(url)
+
+connection.onerror = error => {
+    console.log(`WebSocket error: ${error}`)
+}
+
+connection.onopen = () => {
+    connection.send('hey')
+}
+
+connection.onmessage = e => {
+    showMessage(e.data);
+}
+
+function showMessage(message) {
+    let node = document.createTextNode(message);
+    document.querySelector("#history table")
+            .insertRow(-1)
+            .insertCell(0)
+            .appendChild(node);
+}
+
+console.log("app.js loaded.");

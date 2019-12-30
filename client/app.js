93494dc241c755a5ace949fc8b786f1f34f1d761
Keenan Turley <keenan.turley@gmail.com>
2019-12-30 00:28:07 -0500

    Implement send/receive functionality

--- client/app.js
+++ client/app.js
@@ -3,16 +3,14 @@ const connection = new WebSocket(url)
 
 connection.onerror = error => {
     console.log(`WebSocket error: ${error}`)
-}
-
-connection.onopen = () => {
-    connection.send('hey')
+    showMessage("Error: Failed to connect to server.");
 }
 
 connection.onmessage = e => {
     showMessage(e.data);
 }
 
+// Shows a message in the history
 function showMessage(message) {
     let node = document.createTextNode(message);
     document.querySelector("#history table")
@@ -20,5 +18,19 @@ function showMessage(message) {
             .insertCell(0)
             .appendChild(node);
 }
+
+// Sends a message to the server via websocket
+function sendMessage(message) {
+    connection.send(message);
+}
+
+// Hook command line
+let commandLine = document.getElementById("command-line");
+commandLine.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        sendMessage(commandLine.value);
+        commandLine.value = "";
+    }
+});
 
 console.log("app.js loaded.");
--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
-import { Server } from 'ws';
-
-const wss = new Server({ port: 8080 })
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('ho!')
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`)
+        wss.broadcast(message);
+    });
 })
+
+wss.broadcast = (data) => {
+    wss.clients.forEach((client) => {
+        client.send(data);
+    });
+};
